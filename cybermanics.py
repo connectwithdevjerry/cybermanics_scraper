256f1dce7c1c4c14f6700934425e47aef2959bbc
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-05-14 23:56:18 +0100

    just commit - cybermanics

--- cybermanics.py
+++ cybermanics.py
@@ -78,42 +78,42 @@ def sugar_beach():
     end = time.time()
     print(f'the time used is {end-start}s')
 
-# def grace_bay_club():
-#     global driver, num
-#     website = f"https://be.synxis.com/?_ga-ft=1bCFhE.0.0.0.0.3ZPUE9-5h0-4io-BlJ-Akmnwggi.0.0&_gl=1*1pw9ceo*_ga*MTIwMjEwNTkwMy4xNjk3NTA0NjEy*_ga_FDTY66CS39*MTY5NzcwODU5MS4yLjEuMTY5NzcwODc0NC41Ni4wLjA.&adult=1&arrive={todays_date}&chain=24447&child=0&config=leading1&currency=USD&depart={tomorrow}&hotel=6905&level=hotel&locale=en-US&roomcategory=JR%2C1BR%2C2BR%2C3BR%2C4BR%2C5BR&rooms=1&src=30&theme=leading1"
-#     driver.get(website)
-#     # btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-controls="select-dropdown-wrapper-view-results-by-room-rate"]')
-#     # btn.click()
-#     driver.implicitly_wait(30)
-#     itr = get_room_divs('gracebay')
-#     rooms = []
-#     hotel_name = driver.find_element(By.ID, "heroTitle").text
-#     for div in itr:
-#         room_divs = get_room_divs('gracebay')
-#         room_type = room_divs[num].find_element(By.XPATH, "//div[@class='thumb-cards_roomCategory']//div[@class='thumb-cards_header']//h2[@class='app_heading1']").text
-#         room_name = room_divs[num].find_element(By.XPATH, "//div[@class='app_col-sm-12 app_col-md-8 app_col-lg-8']//div[@class='thumb-cards_cardHeader']//h3").text
-#         roomsize_guest = room_divs[num].find_element(By.XPATH, "//div[@class='guests-and-roomsize_item guests-and-roomsize_guests']//span").text
-#         roomsize_bed = room_divs[num].find_element(By.XPATH, "//div[@class='guests-and-roomsize_item guests-and-roomsize_bed']//span").text
-#         beachEscape_price = room_divs[num].find_element(By.XPATH, "//div[@class='thumb-cards_priceContainer']//ins").text
-#         standardRate_price = room_divs[num].find_element(By.XPATH, "//div[@class='thumb-cards_price']//span").text
-#         roomsize_size = room_divs[num].find_element(By.XPATH, "//div[@class='guests-and-roomsize_item guests-and-roomsize_size']").text.replace("\nsquare feet", "")
-#         print(room_type, room_name, roomsize_guest, roomsize_bed, roomsize_size, beachEscape_price, standardRate_price)
+def grace_bay_club():
+    global driver, num
+    website = f"https://be.synxis.com/?_ga-ft=1bCFhE.0.0.0.0.3ZPUE9-5h0-4io-BlJ-Akmnwggi.0.0&_gl=1*1pw9ceo*_ga*MTIwMjEwNTkwMy4xNjk3NTA0NjEy*_ga_FDTY66CS39*MTY5NzcwODU5MS4yLjEuMTY5NzcwODc0NC41Ni4wLjA.&adult=1&arrive={todays_date}&chain=24447&child=0&config=leading1&currency=USD&depart={tomorrow}&hotel=6905&level=hotel&locale=en-US&roomcategory=JR%2C1BR%2C2BR%2C3BR%2C4BR%2C5BR&rooms=1&src=30&theme=leading1"
+    driver.get(website)
+    # btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-controls="select-dropdown-wrapper-view-results-by-room-rate"]')
+    # btn.click()
+    driver.implicitly_wait(30)
+    itr = get_room_divs('gracebay')
+    rooms = []
+    hotel_name = driver.find_element(By.ID, "heroTitle").text
+    for div in itr:
+        room_divs = get_room_divs('gracebay')
+        room_type = room_divs[num].find_element(By.XPATH, "//div[@class='thumb-cards_roomCategory']//div[@class='thumb-cards_header']//h2[@class='app_heading1']").text
+        room_name = room_divs[num].find_element(By.XPATH, "//div[@class='app_col-sm-12 app_col-md-8 app_col-lg-8']//div[@class='thumb-cards_cardHeader']//h3").text
+        roomsize_guest = room_divs[num].find_element(By.XPATH, "//div[@class='guests-and-roomsize_item guests-and-roomsize_guests']//span").text
+        roomsize_bed = room_divs[num].find_element(By.XPATH, "//div[@class='guests-and-roomsize_item guests-and-roomsize_bed']//span").text
+        beachEscape_price = room_divs[num].find_element(By.XPATH, "//div[@class='thumb-cards_priceContainer']//ins").text
+        standardRate_price = room_divs[num].find_element(By.XPATH, "//div[@class='thumb-cards_price']//span").text
+        roomsize_size = room_divs[num].find_element(By.XPATH, "//div[@class='guests-and-roomsize_item guests-and-roomsize_size']").text.replace("\nsquare feet", "")
+        print(room_type, room_name, roomsize_guest, roomsize_bed, roomsize_size, beachEscape_price, standardRate_price)
 
-#         output = {
-#             'hotel_name': hotel_name,
-#             'room_type': room_type,
-#             'no_of_rooms': 1,
-#             'rooms': [{
-#                 'room_name': room_name,
-#                 'roomsize_guest': roomsize_guest,
-#                 'roomsize_bed': roomsize_bed,
-#                 'roomsize_size': roomsize_size,
-#                 'beachEscape_price': beachEscape_price,
-#                 'standardRate_price': standardRate_price,
-#             }]
-#         }
-#         print(output)
-#         num+=1
+        output = {
+            'hotel_name': hotel_name,
+            'room_type': room_type,
+            'no_of_rooms': 1,
+            'rooms': [{
+                'room_name': room_name,
+                'roomsize_guest': roomsize_guest,
+                'roomsize_bed': roomsize_bed,
+                'roomsize_size': roomsize_size,
+                'beachEscape_price': beachEscape_price,
+                'standardRate_price': standardRate_price,
+            }]
+        }
+        print(output)
+        num+=1
 
 
 
